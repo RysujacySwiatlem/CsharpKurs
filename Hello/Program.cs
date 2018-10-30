71d9b7a590ddb1454a66526555e51960d8d6fe2c
Kamil St <rysowaneswiatlem@gmail.com>
2018-10-30 09:31:09 +0100

    Hello 1.1

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,14 +6,48 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            
-            string name = args[0];
-            Console.WriteLine("Witaj " + name);
-            foreach (var item in args)
+            for (; ; )
             {
-                Console.WriteLine("Witaj " + item);
+                Imie();
+                Wiek();
+                Koniec();
             }
+        }
 
+        private static void Koniec()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+        /// <summary>
+        /// podanie wieku, działanie zależne od wieku
+        /// </summary>
+        private static void Wiek()
+        {
+            Console.Write("Podaj swój wiek: ");
+            int wiek;
+            bool wynik = int.TryParse(Console.ReadLine(), out wiek);
+            if (wiek >= 18)
+            {
+                Console.WriteLine("Masz, napij się browara");
+            }
+            else if (wynik == false)
+            {
+                Console.WriteLine("Musisz wprowadzić liczbę");
+            }
+            else
+            {
+                Console.WriteLine("Proszę, oto Twoja herbata");
+            }
+        }
+        /// <summary>
+        /// Prośba o padanie imienia
+        /// </summary>
+        private static void Imie()
+        {
+            Console.Write("Wpisz swoje imie: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine("Witaj " + imie);
         }
     }
 }
